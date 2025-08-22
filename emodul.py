4b884b69dea644866f9dbc12b47ddf526df6f223
emodulTerbaru <emodulfives@gmail.com>
2025-08-22 21:35:35 +0700

    Update emodul.py

--- emodul.py
+++ emodul.py
@@ -705,7 +705,7 @@ if st.session_state.kelompok['kondisi4']:
         st.rerun()
         
 def soal_adaptif():
-    st.markdown(""" <div style="display:flex; justify-content:space-evenly;align-items:center; border:2px solid white; margin-bottom:10px; padding:2px;">
+    st.markdown(""" <div style="display:flex; justify-content:space-evenly;align-items:center; border:2px solid white; margin-bottom:10px; padding:2px;background-color:grey">
                     <img src='https://i.pinimg.com/originals/48/59/55/485955115c68020ce3ba16bf18a95d8a.gif' style='width:100px;'></img>
                     <div class="label">Level Pertama</div>
                     </div>""",unsafe_allow_html=True)
@@ -739,7 +739,7 @@ def soal_adaptif():
 if st.session_state.adaptif:
     soal_adaptif()
 def level_kedua():
-    st.markdown(""" <div style="display:flex; justify-content:space-evenly;align-items:center; border:2px solid white; margin-bottom:10px; padding:2px;">
+    st.markdown(""" <div style="display:flex; justify-content:space-evenly;align-items:center; border:2px solid white; margin-bottom:10px; padding:2px; background-color:grey">
                     <img src='https://i.pinimg.com/originals/48/59/55/485955115c68020ce3ba16bf18a95d8a.gif' style='width:100px;'></img>
                     <div class="label">Level Kedua</div>
                     </div>""",unsafe_allow_html=True)
@@ -772,7 +772,7 @@ if st.session_state.pengecekan1:
     level_kedua()
 
 def level_ketiga():
-    st.markdown(""" <div style="display:flex; justify-content:space-evenly;align-items:center; border:2px solid white; margin-bottom:10px; padding:2px;">
+    st.markdown(""" <div style="display:flex; justify-content:space-evenly;align-items:center; border:2px solid white; margin-bottom:10px; padding:2px;background-color:grey">
                     <img src='https://i.pinimg.com/originals/48/59/55/485955115c68020ce3ba16bf18a95d8a.gif' style='width:100px;'></img>
                     <div class="label">Level Ketiga</div>
                     </div>""",unsafe_allow_html=True)
@@ -1022,6 +1022,7 @@ if st.session_state.kelompok['kondisi11']:
         st.session_state.kelompok['kondisi12']=True
         st.rerun()
     
+
 
 
 
